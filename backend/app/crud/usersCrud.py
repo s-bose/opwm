00c36ecad121f374148ae3700903c41cfb156946
s-bose <shiladitya.31.z@gmail.com>
2021-04-05 00:46:52 +0530

    - using alembic

---
+++ backend/app/crud/usersCrud.py
@@ -0,0 +1,4 @@
+
+
+def get_user(email: str, password: str):
+    pass
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
 
-from app.db import Base
 from app.api import router as main_router
-from app.db import engine
-# TODO - this is bad, use alembic maybe?
-from app.models import User, Passwords
 
-from app.core.config import DATABASE_URL
 app = FastAPI()
 
-print(DATABASE_URL)
 app.include_router(main_router, prefix="/api")
-
-Base.metadata.create_all(bind=engine)
