921edcf1a0fca17182d613bffc3a97270f31b906
Shiladitya Bose <shiladitya_basu@live.com>
2021-05-08 01:01:43 +0530

    init frontend (again)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@
   <router-view/>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,14 +17,14 @@
 
 #nav {
   padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
