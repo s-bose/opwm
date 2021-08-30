1a77ccb64d07bc4aa10fe7652e6335e9375fe507
Shiladitya Bose <shiladitya.31.z@gmail.com>
2021-08-31 01:05:32 +0530

    started working on list view

---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- <div class="py-16">
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-red-900' : 'bg-red-700'"
+      class="
+        relative
+        inline-flex
+        items-center
+        h-6
+        rounded-full
+        w-11
+        transition-colors
+        ease-in-out
+        duration-200
+        focus:outline-none
+        focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75
+      "
+    >
+      <span class="sr-only">Enable notifications</span>
+      <span
+        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block w-4 h-4 transform bg-white rounded-full ring-0 transition ease-in-out duration-200"
+        s
+      />
+    </Switch>
+  </div> -->
+
+  <div class="container">
+    <button class="bg-red-700 text-white h-12 w-12" @click="showPop = !showPop">click me</button>
+    <delete-modal v-model:showModal="showPop" site="Google" />
+    <!-- <password-modal :isEditorMode="false" v-model:showModal="showPop" @newPassword="showPassObj" /> -->
+
+    <!-- <ModalGeneric :modelValue="showPop" @update:modelValue="showPop = $event" /> -->
+  </div>
+</template>
+
+<script>
+// import { ref } from "vue";
+// import { Switch } from "@headlessui/vue";
+import DeleteModal from "../components/DeleteModalComponent.vue";
+export default {
+  name: "Test",
+  components: { DeleteModal },
+  data() {
+    return {
+      showPop: false,
+    };
+  },
+
+  updated() {
+    // console.log(this.showPop);
+  },
+  // components: { Switch },
+
+  // setup() {
+  //   const enabled = ref(true);
+  //   return { enabled };
+  // },
+  methods: {
+    showPassObj(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
